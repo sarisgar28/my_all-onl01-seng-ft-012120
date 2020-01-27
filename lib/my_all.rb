2719eaa38e8fcded6ae4ef327fc09eb88d0afbe7
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 21:29:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
-def my_all?(collection)
-
-end
+def my_all?(["1,2,3,"]) {|i| i < 2}
+  i = 0
+  block_return-values = []
+  while i  <
+  collection.length 
+   block_return-values << yield (collection [i])
+   i = i + 1
+   puts block_return_values.include? = [
+     (true, false, false]
+  else 
+    true 
+  end 
+  end
